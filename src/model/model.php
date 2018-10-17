65543ab56597bf81df9046f9754a28a4c34e0e5c
Nicholas Rianto Putra <nicholasmagbanua@gmail.com>
2018-10-17 12:38:32 +0700

    prevent SQL Injection

--- src/model/model.php
+++ src/model/model.php
@@ -1,12 +1,13 @@
 <?php
-include('../../config.php');
+include('dbconnection.class.php');
 $db = new DBConnection('localhost', 'probook', 'root', 'Nicho01');
+// $login =
 // $db->getAllUsers();
-// if ($db->check_login('cebong', 'bowobangsa') == 1) {
-//     echo 'ememek';
-// } else {
-//     echo 'asu';
-// }
+if ($db->check_login("cebong", 'bowobangsat') == 1) {
+    echo 'memek';
+} else {
+    echo 'asu';
+}
 // $query = $dbh->query("SELECT * FROM Users");
 // while ($row = $query->fetch()) {
 //     echo "<br>".print_r($row)."<br />\n";
