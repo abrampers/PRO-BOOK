18c5782935f04961f70a8dc9bc67cdc1359cd993
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-17 11:14:04 +0700

    Initial Templating Engine

--- index.php
+++ index.php
@@ -2,23 +2,30 @@
 include_once 'lib/request/Request.php';
 include_once 'lib/router/Router.php';
 include_once 'src/controller/middleware/TestMiddleware.php';
+include_once 'src/view/login.php';
 $router = new Router(new Request);
 
 $router->get('/', function($request) {
-    include('huyu.html');
-});
-
-$router->get('/profile', function($request) {
     return <<<HTML
-    <h1>Profile</h1>
+    <h1>Hello World</h1>
 HTML;
 });
 
-$router->get('/test', function($request) {
-    return '<h1>TEST BERHASIL HUYUUUU</h1>';
-},
-new TestMiddleware
-);
+$router->get('/login', function($request) {
+    return renderLoginView("huyuuu");
+});
+
+// $router->get('/profile', function($request) {
+//     return <<<HTML
+//     <h1>Profile</h1>
+// HTML;
+// });
+
+// $router->get('/test', function($request) {
+//     return '<h1>TEST BERHASIL HUYUUUU</h1>';
+// },
+// new TestMiddleware
+// );
 
 $router->post('/data', function($request) {
     return json_encode($request->getBody());
