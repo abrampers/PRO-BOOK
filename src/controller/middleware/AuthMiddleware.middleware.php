ce400209c445394d9fd845e00539647f0cf7102a
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-17 13:36:57 +0700

    Created Authentication Middleware

---
+++ src/controller/middleware/AuthMiddleware.middleware.php
@@ -0,0 +1,10 @@
+<?php
+include_once "lib/router/MiddlewareInterface.interface.php";
+include_once "lib/request/Request.class.php";
+include_once "model/MarufDB.class.php";
+class AuthMiddleware implements MiddlewareInterface {
+  public function run(Closure $callback, Request $request) {
+
+    if()
+  }
+}
