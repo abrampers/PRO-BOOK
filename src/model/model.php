319b5ca5d4dc9b0f0dccf8b8f70efb134818bca0
Nicholas Rianto Putra <nicholasmagbanua@gmail.com>
2018-10-17 10:59:45 +0700

    add class DBConnection

---
+++ src/model/model.php
@@ -0,0 +1,13 @@
+<?php
+include('../../config.php');
+$db = new DBConnection('localhost', 'probook', 'root', 'Nicho01');
+// $db->getAllUsers();
+// if ($db->check_login('cebong', 'bowobangsa') == 1) {
+//     echo 'ememek';
+// } else {
+//     echo 'asu';
+// }
+// $query = $dbh->query("SELECT * FROM Users");
+// while ($row = $query->fetch()) {
+//     echo "<br>".print_r($row)."<br />\n";
+// }
