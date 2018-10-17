6f66a856f2df90004c3fc47cb140e5f33e0967a7
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-17 12:34:20 +0700

    HTTP POST method works

--- index.php
+++ index.php
@@ -6,27 +6,21 @@ include_once 'src/view/login.php';
 $router = new Router(new Request);
 
 $router->get('/', function($request) {
-    return <<<HTML
-    <h1>Hello World</h1>
+  return <<<HTML
+  <h1>Hello World</h1>
 HTML;
 });
 
 $router->get('/login', function($request) {
-    return renderLoginView("huyuuu");
+  return renderLoginView("huyuuu");
 });
 
-// $router->get('/profile', function($request) {
-//     return <<<HTML
-//     <h1>Profile</h1>
-// HTML;
-// });
-
-// $router->get('/test', function($request) {
-//     return '<h1>TEST BERHASIL HUYUUUU</h1>';
-// },
-// new TestMiddleware
-// );
+$router->get('/loginh', function($request) {
+  header('Location: http://localhost:5000/login');
+  exit();
+});
 
 $router->post('/data', function($request) {
-    return json_encode($request->getBody());
+  print_r($_POST);
+  return json_encode($request->getBody());
 });
