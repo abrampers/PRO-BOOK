5717eb99dcf34395af288a2bc79bd5cfaf5c6464
Nicholas Rianto Putra <nicholasmagbanua@gmail.com>
2018-10-19 00:49:16 +0700

    backend registeR

--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -5,7 +5,7 @@ class RegisterController implements ControllerInterface {
       $db = new MarufDB($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
       $result = $db->addProfile($request->name, $request->username, $request->email, $request->password, $request->address, $request->phonenumber);
       if ($result == 1) {
-
+        return LoginController::control($request);
       } else {
         $template = new Template('src/view/register.php');
         return $template->render();
@@ -13,22 +13,6 @@ class RegisterController implements ControllerInterface {
     } else {
       $template = new Template('src/view/register.php');
       return $template->render();
-    }
-    $db = new MarufDB($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-    $user_id = $db->checkLogin($request->username, $request->password);
-    if($user_id != -1) {
-      $JKWToken = new JKWToken();
-      $token = $JKWToken->generateJKWToken();
-      if ($db->addToken($user_id, $token) == 1) {
-        setcookie("token", $token, time() + (int)$_ENV['COOKIE_EXPIRED_TIME'], '/');
-        header("Location: http://{$request->serverName}:{$request->serverPort}/");
-        exit();
-      } else {
-        return '<h1>Failed</h1>';
-      }
-    } else {
-      $template = new Template('src/view/login.php');
-      return $template->render(True);
     }
   }
 }
