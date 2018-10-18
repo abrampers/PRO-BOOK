067fec62e4a6fafc77b3f0b67b766c1769456c70
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-18 18:15:34 +0700

    Merge conflict

--- index.php
+++ index.php
@@ -5,58 +5,62 @@ include_once 'lib/template_engine/Template.class.php';
 include_once 'src/controller/middleware/TestMiddleware.middleware.php';
 include_once 'src/controller/middleware/AuthMiddleware.middleware.php';
 include_once 'src/controller/middleware/TokenValidationMiddleware.middleware.php';
+include_once 'src/controller/middleware/LoginRegisterMiddleware.middleware.php';
 include_once 'src/controller/LoginController.php';
 include_once 'src/controller/api.php';
 $router = new Router(new Request);
 
-$router->get('/', function($request) {
-  return <<<HTML
-  <h1>Main Page HUYU</h1>
-HTML;
-});
 
 /***************/
 /* ROUTE PATHS */
 /************* */
 
 /** GET */
+$router->get('/', function($request) {
+  return <<<HTML
+  <h1>Main Page HUYU</h1>
+HTML;
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
+
 $router->get('/login', function($request) {
   $template = new Template('src/view/login.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new LoginRegisterMiddleware]);
 
 $router->get('/register', function($request) {
   $template = new Template('src/view/register.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new LoginRegisterMiddleware]);
 
 $router->get('/browse', function($request) {
-  $template = new Template('src/view/register.php');
+  $template = new Template('src/view/huyu.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
 
 $router->get('/history', function($request) {
-  $template = new Template('src/view/register.php');
+  $template = new Template('src/view/huyu.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
 
 $router->get('/history/review', function($request) {
-  $template = new Template('src/view/register.php');
+  $template = new Template('src/view/huyu.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
 
 $router->get('/profile', function($request) {
-  $template = new Template('src/view/register.php');
+  $template = new Template('src/view/huyu.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
 
 $router->get('/profile/edit', function($request) {
-  $template = new Template('src/view/register.php');
+  $template = new Template('src/view/huyu.php');
   return $template->render();
-});
+}, [new TokenValidationMiddleware, new AuthMiddleware]);
 
 $router->get('/deletecookie', function($request) {
-  setcookie('token', '', time() - 3600);
+  $db = new MarufDB('localhost', 'probook', 'root', '');
+  $db->deleteToken($request->token);
+  setcookie('token', '', time() - 3600, '/');
 });
 
 /** POST */
--- src/controller/middleware/AuthMiddleware.middleware.php
+++ src/controller/middleware/AuthMiddleware.middleware.php
@@ -5,7 +5,6 @@ include_once "src/model/MarufDB.class.php";
 class AuthMiddleware implements MiddlewareInterface {
   public function run(Request $request) {
     $token = $_COOKIE['token'];
-    $db = new MarufDB('localhost', 'probook', 'root', '');
     if (is_null($token)) {
       header("Location: http://{$request->serverName}:{$request->serverPort}/login");
       return False;
