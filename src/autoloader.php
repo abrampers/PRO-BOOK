098e6084fd427484a92cb180aba58297f57aebb3
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-18 23:37:23 +0700

    Added autoloader

--- src/autoloader.php
+++ src/autoloader.php
@@ -3,7 +3,16 @@ spl_autoload_register(function($className) {
   $extensions = array(".php");
   $root = $_SERVER['DOCUMENT_ROOT'];
   $paths = array(
-    '/lib/', '/lib/dotethes/', '/lib/jkwtoken/', '/lib/request/', '/lib/router/', '/lib/template_engine/', '/src/', '/src/controller/', '/src/controller/middleware/', '/src/model'
+    '/lib/',
+    '/lib/dotethes/',
+    '/lib/jkwtoken/',
+    '/lib/request/',
+    '/lib/router/',
+    '/lib/template_engine/',
+    '/src/',
+    '/src/controller/',
+    '/src/controller/middleware/',
+    '/src/model'
   );
   foreach ($paths as $path) {
     $filename = $root . $path . DIRECTORY_SEPARATOR . $className;
