57a87401c7c72b39eacbeae9eb55ccf6215ee607
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-24 14:47:24 +0700

    Fixed camelcase bug

--- src/controller/RegisterPostController.php
+++ src/controller/RegisterPostController.php
@@ -2,7 +2,7 @@
 class RegisterPostController implements ControllerInterface {
   public static function control(Request $request) {
     $db = new MarufDB();
-    $result = $db->addProfile($request->name, $request->username, $request->email, $request->password, $request->address, $request->phonenumber);
+    $result = $db->addProfile($request->name, $request->username, $request->email, $request->password, $request->address, $request->phoneNumber);
     if ($result == 1) {
       return LoginPostController::control($request);
     } else {
