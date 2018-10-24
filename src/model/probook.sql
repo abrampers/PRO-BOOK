199a14f3a531a1e669859e43c1d2e0d97401b4b5
Nicholas Rianto Putra <nicholasmagbanua@gmail.com>
2018-10-24 16:11:52 +0700

    dump new database

--- src/model/probook.sql
+++ src/model/probook.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
 --
 -- Host: localhost    Database: probook
 -- ------------------------------------------------------
--- Server version	5.7.23-0ubuntu0.16.04.1
+-- Server version	5.7.24-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -35,7 +35,7 @@ CREATE TABLE `ActiveTokens` (
 
 LOCK TABLES `ActiveTokens` WRITE;
 /*!40000 ALTER TABLE `ActiveTokens` DISABLE KEYS */;
-INSERT INTO `ActiveTokens` VALUES (3,'6bac7403524fac85272e9b93b17c290f',1539885074),(3,'027996d5212489d8ba51946020ff0632',1539885107),(3,'7329a41622a4b6ee7339b9c0d8627dfe',1540211593),(3,'b1c210b621b0eb19c95897ea807ee6d3',1540284177),(3,'e246131327306f2eb4196336a2f21706',1540285956),(3,'6b996061cc9685adf27019bf39b560e5',1540287196);
+INSERT INTO `ActiveTokens` VALUES (3,'6bac7403524fac85272e9b93b17c290f',1539885074),(3,'027996d5212489d8ba51946020ff0632',1539885107),(3,'7329a41622a4b6ee7339b9c0d8627dfe',1540211593),(3,'b1c210b621b0eb19c95897ea807ee6d3',1540284177),(3,'e246131327306f2eb4196336a2f21706',1540285956),(3,'6b996061cc9685adf27019bf39b560e5',1540287196),(3,'80a6ea11845981f907e527fb0b3434f5',1540361324),(3,'062d2da5ca9f1bd4e442158fdf4e4ccb',1540361486),(3,'9d3f6cc9f1b46077fcf7c3f16befaf4e',1540361818),(2,'bd381a66553ffc56c4a9d7d81755bb85',1540367581),(1,'7841d2c03c0def73be8622f6f31eb126',1540367711),(1,'907efc4f7283a2d1261cdb0b8f081b5c',1540367751),(3,'19dd89f90b4ca4c2e8ce876858987a17',1540370723);
 /*!40000 ALTER TABLE `ActiveTokens` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +64,7 @@ CREATE TABLE `Books` (
 
 LOCK TABLES `Books` WRITE;
 /*!40000 ALTER TABLE `Books` DISABLE KEYS */;
-INSERT INTO `Books` VALUES (1,'The Communist Manifesto','Karl Marx','The Communist Manifesto is divided into a preamble and four sections, the last of these a short conclusion.',NULL,0,0),(2,'The Cold War: A New History','John Lewis Gaddis','The dean of Cold War historians (The New York Times) now presents the definitive account of the global confrontation that dominated the last half of the twentieth century.',NULL,0,0);
+INSERT INTO `Books` VALUES (1,'The Communist Manifesto','Karl Marx','The Communist Manifesto is divided into a preamble and four sections, the last of these a short conclusion.',NULL,5,1),(2,'The Cold War: A New History','John Lewis Gaddis','The dean of Cold War historians (The New York Times) now presents the definitive account of the global confrontation that dominated the last half of the twentieth century.',NULL,0,0);
 /*!40000 ALTER TABLE `Books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +83,7 @@ CREATE TABLE `Orders` (
   `amount` int(11) DEFAULT NULL,
   `order_timestamp` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +92,7 @@ CREATE TABLE `Orders` (
 
 LOCK TABLES `Orders` WRITE;
 /*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
-INSERT INTO `Orders` VALUES (1,3,0,1,1,1540284177),(2,3,1,2,4,1542287196);
+INSERT INTO `Orders` VALUES (1,3,1,1,1,1540284177),(2,3,1,2,4,1542287196),(8,3,0,1,1,1540362838),(9,3,0,1,3,1540362861);
 /*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +110,7 @@ CREATE TABLE `Reviews` (
   `book_id` int(11) DEFAULT NULL,
   `username` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,6 +119,7 @@ CREATE TABLE `Reviews` (
 
 LOCK TABLES `Reviews` WRITE;
 /*!40000 ALTER TABLE `Reviews` DISABLE KEYS */;
+INSERT INTO `Reviews` VALUES (2,5,'ASU KAMU MEMEK BOWO ANJING',1,'skidipapap');
 /*!40000 ALTER TABLE `Reviews` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +149,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'Koko widodo','cebong','cebongarmy@kokowi.com','bowobangsat','istana presiden 100','08136969669',NULL),(2,'Erick Thohir','sayacebong','asu@gmail.com','64ca1296793ddd885bb6ceed0a9aa70e','san siro','123456789',NULL),(3,'Meki Kuda','skidipapap','kemem@gmail.com','cc0f8df45236d57a0ea3eb700a97b07b','Thailand Banci 120','081999999',NULL);
+INSERT INTO `Users` VALUES (1,'Koko Widodo','cebong','cebongarmy@kokowi.com','58cebe057320fd1e5a7e96ab7e4172eb','istana presiden 10','08136969669',NULL),(2,'Erick Thohir','sayacebong','asu@gmail.com','64ca1296793ddd885bb6ceed0a9aa70e','san siro','123456789',NULL),(3,'Kimcil Kepolen','skidipapap','kemem@gmail.com','cc0f8df45236d57a0ea3eb700a97b07b','Thailand Banci 120','081999999',NULL);
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -161,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-23 20:46:15
+-- Dump completed on 2018-10-24 16:11:35
