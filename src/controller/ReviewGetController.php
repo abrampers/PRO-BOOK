b217316927a8fc6a028433d30dc0ffb580a51edb
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-24 15:32:30 +0700

    Finished Review functionality

--- src/controller/ReviewGetController.php
+++ src/controller/ReviewGetController.php
@@ -1,12 +1,12 @@
 <?php
-class RatingGetController implements ControllerInterface {
+class ReviewGetController implements ControllerInterface {
   public static function control(Request $request) {
     $db = new MarufDB();
     $token = $_COOKIE['token'];
     $book = $db->getBookDetail($request->id);
     $user_id = $db->getUserId($token);
     $username = $db->getUsername($token);
-    $template = new Template('src/view/rating.php');
-    return $template->render($book, $username, $user_id);
+    $template = new Template('src/view/review.php');
+    return $template->render($username, $book, $user_id);
   }
 }
