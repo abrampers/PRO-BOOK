9e62d66dee19b8a1d5aad07c9993f219277b4e48
Faza Fahleraz <ffahleraz@gmail.com>
2018-10-25 17:41:07 +0700

    Change stars rating to change value only when clicked

--- src/view/static/js/review.js
+++ src/view/static/js/review.js
@@ -1,18 +1,51 @@
 import $$ from './lib/jQowi.js';
 
-function updateStarState(activeStarNum) {
-  $$('#ratingField').value = activeStarNum;
+function hoverStars(index) {
   $$('.review-star').forEach((element) => {
-    if (parseInt(element.id) <= activeStarNum) {
-      element.classList.add('filled');
+    element.classList.remove('pre-selected');
+    element.classList.remove('pre-removed');
+    if (parseInt(element.id) <= index) {
+      if (!element.classList.contains('selected')) element.classList.add('pre-selected');
     } else {
-      element.classList.remove('filled');
+      if (element.classList.contains('selected')) {
+        element.classList.remove('selected');
+        element.classList.add('pre-removed');
+      }
+    }
+  });
+}
+
+function unhoverStars() {
+  $$('.review-star').forEach((element) => {
+    element.classList.remove('pre-selected');
+    if (element.classList.contains('pre-removed')) {
+      element.classList.remove('pre-removed');
+      element.classList.add('selected');
+    }
+  });
+}
+
+function selectStars(index) {
+  $$('#ratingField').value = index;
+  $$('.review-star').forEach((element) => {
+    element.classList.remove('pre-selected');
+    element.classList.remove('pre-removed');
+    if (parseInt(element.id) <= index) {
+      element.classList.add('selected');
+    } else {
+      element.classList.remove('selected');
     }
   });
 }
 
 $$('.review-star').forEach((element) => {
   element.onmouseenter = () => {
-    updateStarState(parseInt(element.id));
+    hoverStars(parseInt(element.id));
+  };
+  element.onmouseleave = () => {
+    unhoverStars();
+  };
+  element.onclick = () => {
+    selectStars(parseInt(element.id));
   };
 });
